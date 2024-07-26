b526d0114e7ac04181bc73e273a019f0659edee4
Kauan <kauanlucastoldo@gmail.com>
2024-07-26 09:19:03 -0300

    Design tela de login

--- project/lib/main.dart
+++ project/lib/main.dart
@@ -15,10 +15,12 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  bool _lembrarDeMim = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 18, 18, 18),
+      backgroundColor: const Color.fromARGB(255, 18, 18, 18),
       body: Padding(
         padding: const EdgeInsets.all(50.0),
         child: Center(
@@ -33,8 +35,8 @@ class _LoginPageState extends State<LoginPage> {
                       'assets/logo.png',
                       scale: 3.5,
                     ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 40),
+                    const Padding(
+                      padding: EdgeInsets.only(top: 40),
                       child: Text(
                         "Entrar no Spotfree",
                         style: TextStyle(
@@ -45,72 +47,90 @@ class _LoginPageState extends State<LoginPage> {
                     ),
                   ],
                 ),
-                Container(
-                  child: Column(
-                    children: [
-                      TextField(
-                        style: TextStyle(color: Colors.white),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      "Nome de usuário",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16),
+                      textAlign: TextAlign.left,
+                    ),
+                    TextField(
+                      style: const TextStyle(color: Colors.white),
+                      decoration: InputDecoration(
+                        filled: false,
+                        labelStyle: const TextStyle(color: Colors.white),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 16.0,
+                    ),
+                    const Text(
+                      "Senha",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    TextField(
+                        obscureText: true,
+                        style: const TextStyle(color: Colors.white),
                         decoration: InputDecoration(
                           filled: false,
-                          labelStyle: TextStyle(color: Colors.white),
+                          labelStyle: const TextStyle(color: Colors.white),
                           border: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(8.0),
                           ),
+                        )),
+                    const SizedBox(height: 16.0),
+                    Row(
+                      children: [
+                        Switch(
+                          value: _lembrarDeMim,
+                          onChanged: (value) {
+                            setState(() {
+                              _lembrarDeMim = value;
+                            });
+                          },
+                          activeColor: const Color.fromARGB(255, 30, 215, 96),
                         ),
-                      ),
-                      SizedBox(
-                        height: 16.0,
-                      ),
-                      TextField(
-                          obscureText: true,
+                        const Text(
+                          'Lembrar de mim',
                           style: TextStyle(color: Colors.white),
-                          decoration: InputDecoration(
-                            filled: false,
-                            labelStyle: TextStyle(color: Colors.white),
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(8.0),
-                            ),
-                          )),
-                      SizedBox(height: 16.0),
-                      Row(
-                        children: [
-                          Switch(
-                            value: true,
-                            onChanged: (value) {
-                              setState(() {
-                                value = false;
-                              });
-                            },
-                            activeColor: const Color.fromARGB(255, 22, 255, 30),
-                          ),
-                          Text(
-                            'Lembrar de mim',
-                            style: TextStyle(color: Colors.white),
-                          ),
-                        ],
-                      ),
-                      SizedBox(height: 16.0),
-                      ElevatedButton(
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 16.0),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
                         onPressed: () {},
                         style: ElevatedButton.styleFrom(
-                          backgroundColor: Color.fromARGB(255, 11, 249, 15),
-                          padding: EdgeInsets.symmetric(
+                          backgroundColor:
+                              const Color.fromARGB(255, 30, 215, 96),
+                          padding: const EdgeInsets.symmetric(
                               horizontal: 100, vertical: 15),
                           shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(50.0),
                           ),
                         ),
-                        child: Text(
+                        child: const Text(
                           'Entrar',
                           style: TextStyle(
-                              color: const Color.fromARGB(255, 0, 0, 0),
+                              color: Color.fromARGB(255, 0, 0, 0),
                               fontWeight: FontWeight.bold),
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-                Column(
+                const Column(
                   children: [
                     Text(
                       "Made by @toldokauan & @lynxmaah",
